71c4ba84e2b48b699fa5613ea2bf071dd57704b7
sirine.hassine14@gmail.com <sirine.hassine14@gmail.com>
2024-11-05 11:24:06 +0100

    Add Docker files

---
+++ DockerFiles/db/db_backup.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS `message`;
+
+CREATE TABLE `message` (
+ `id` bigint(20) NOT NULL AUTO_INCREMENT,
+`Email` varchar(255),
+`FirstName` varchar(255),
+`LastName` varchar(255),
+`Message` varchar(255),
+PRIMARY KEY(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf-8;
+
