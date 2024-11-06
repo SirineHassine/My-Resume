9adb26f2e1cc57233814140c2b2fd5a557cef811
sirine.hassine14@gmail.com <sirine.hassine14@gmail.com>
2024-11-06 19:55:56 +0100

    correct vars apps conenction

--- src/main/java/fr/myresume/MainController.java
+++ src/main/java/fr/myresume/MainController.java
@@ -1,14 +1,14 @@
 package fr.myresume;
 
 
-import fr.myresume.model.Message;
-import fr.myresume.service.MessageServices;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import fr.myresume.model.Message;
+import fr.myresume.service.MessageServices;
 
 
 @Controller
